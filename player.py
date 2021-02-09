473537f0fcd4861aaac681e5413782eaff28ac49
Raiyan Mahbub <50161218+mahbu006@users.noreply.github.com>
2021-02-09 01:04:24 -0600

    put json data into prelimiary pandas dataframe

--- player.py
+++ player.py
@@ -1,8 +1,9 @@
 import requests
+import pandas as pd
 
-url = "https://football-pro.p.rapidapi.com/api/v2.0/players/search/Lionel%20Messi"
+url = "https://football-pro.p.rapidapi.com/api/v2.0/players/184798"
 
-querystring = {"tz":"Europe/Amsterdam"}
+querystring = {"include":"stats","tz":"Europe/Amsterdam"}
 
 headers = {
     'x-rapidapi-key': "d23bab6737mshb845a2338eedcb2p137711jsnf78fa2d0de2f",
@@ -11,4 +12,5 @@ headers = {
 
 response = requests.request("GET", url, headers=headers, params=querystring)
 
-print(response.text)
+df = pd.read_json(response.text)
+print(df)
