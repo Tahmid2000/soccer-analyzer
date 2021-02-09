3a17530894005e9b288eac57f44e41029333efed
Raiyan Mahbub <50161218+mahbu006@users.noreply.github.com>
2021-02-09 01:35:44 -0600

    organized attribute-values into a pandas df

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+import json
 
 url = "https://football-pro.p.rapidapi.com/api/v2.0/players/184798"
 
@@ -11,6 +12,12 @@ headers = {
     }
 
 response = requests.request("GET", url, headers=headers, params=querystring)
+data = response.json()
+formatted_data = [['Name', data['data']['display_name']],
+                   ['Nationality', data['data']['nationality']],
+                   ['Height', data['data']['height']],
+                   ['Weight', data['data']['weight']]]
 
-df = pd.read_json(response.text)
+df = pd.DataFrame(formatted_data, columns = ['Attribute', 'Value'])
 print(df)
+
