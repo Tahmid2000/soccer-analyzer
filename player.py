0ef67adb2bfc5c9172f70a91729c555c4d2327e5
Raiyan Mahbub <50161218+mahbu006@users.noreply.github.com>
2021-02-09 00:03:41 -0600

    different API endpoint testing

--- player.py
+++ player.py
@@ -1,12 +1,14 @@
 import requests
 
-url = "https://api-football-v1.p.rapidapi.com/v2/players/player/154"
+url = "https://football-pro.p.rapidapi.com/api/v2.0/players/search/Lionel%20Messi"
+
+querystring = {"tz":"Europe/Amsterdam"}
 
 headers = {
     'x-rapidapi-key': "d23bab6737mshb845a2338eedcb2p137711jsnf78fa2d0de2f",
-    'x-rapidapi-host': "api-football-v1.p.rapidapi.com"
+    'x-rapidapi-host': "football-pro.p.rapidapi.com"
     }
 
-response = requests.request("GET", url, headers=headers)
+response = requests.request("GET", url, headers=headers, params=querystring)
 
 print(response.text)
