4c41640212cfd516cc1cecd6f21618bede5d2f21
Tahmid Imran <tahmidimran1@gmail.com>
2021-02-10 00:34:01 -0600

    secret key

--- socceranalyzer/settings.py
+++ socceranalyzer/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0r+tqo#q^fr#e-i79@wm=h&89xn=8&zvab2z@+thdzx5rl&-rx'
-
+SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
