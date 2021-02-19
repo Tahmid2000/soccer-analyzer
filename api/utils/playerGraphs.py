d61b1a270aa57f0face2b95b3326698f00100234
Tahmid Imran <tahmidimran1@gmail.com>
2021-02-18 22:26:44 -0600

    added graphs for player

--- api/utils/playerGraphs.py
+++ api/utils/playerGraphs.py
@@ -4,10 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def playerGraphs(player_id):
-
-    df = getPlayerInfo(player_id)
-
+def attackersGraph(df):
     appearances = df['appearances']
     goals_ratio = df['goals'] / appearances
     assists_ratio = df['assists'] / appearances
@@ -15,24 +12,30 @@ def playerGraphs(player_id):
     successful_dribbles_ratio = df['successful_dribbles'] / appearances
     successful_crosses_ratio = df['successful_crosses'] / appearances
 
-    player_dataframe = [goals_ratio, assists_ratio, key_passes_ratio, successful_dribbles_ratio, successful_crosses_ratio]
-    index = ['Goals', 'Assists', 'Key Passes', 'Successful Dribbles', 'Successful Crosses']
-
-    charts_df = pd.DataFrame(player_dataframe, index=index)
-
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
-    x = np.linspace(0,appearances + 200,50)
-
+    x = np.linspace(0, appearances + 200, 50)
     ax.xaxis.set_ticks_position('bottom')
     ax.yaxis.set_ticks_position('left')
-    markers_on = [100, 200, 500, 1000]
-    plt.plot(x, goals_ratio*x, '-r.', label='Goals', markevery=10)
-    plt.plot(x, assists_ratio*x,'-g', label='Assists')
-    plt.plot(x, key_passes_ratio*x,'-b', label='Key Passes')
-    plt.plot(x, successful_dribbles_ratio*x,'-m', label='Dribbles')
-    plt.plot(x, successful_crosses_ratio*x,'-c', label='Crosses')
+    xthresh = appearances
 
+    below = x <= appearances
+    above = x > appearances - 40
+    y = goals_ratio*x
+    plt.plot(x[below], y[below], '-r', label='Goals', markevery=10)
+    plt.plot(x[above], y[above], '--r',  markevery=10)
+    y = assists_ratio*x
+    plt.plot(x[below], y[below], '-g', label='Assists')
+    plt.plot(x[above], y[above], '--g')
+    y = key_passes_ratio*x
+    plt.plot(x[below], y[below], '-b', label='Key Passes')
+    plt.plot(x[above], y[above], '--b')
+    y = successful_dribbles_ratio*x
+    plt.plot(x[below], y[below], '-m', label='Dribbles')
+    plt.plot(x[above], y[above], '--m')
+    y = successful_crosses_ratio*x
+    plt.plot(x[below], y[below], '-c', label='Crosses')
+    plt.plot(x[above], y[above], '--c')
     plt.xlabel('Appearances')
     plt.ylabel('Statistics')
     plt.legend(loc='upper left')
@@ -40,25 +43,95 @@ def playerGraphs(player_id):
     plt.show()
 
 
+def defendersGraph(df):
+    appearances = df['appearances']
+    tackles_ratio = df['tackles'] / appearances
+    clean_sheets_ratio = df['clean_sheets'] / appearances
+    successful_duels_ratio = df['successful_duels'] / appearances
+    fouls_committed_ratio = df['fouls_committed'] / appearances
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    x = np.linspace(0, appearances + 200, 50)
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+    xthresh = appearances
+
+    below = x <= appearances
+    above = x > appearances - 40
+    y = tackles_ratio*x
+    plt.plot(x[below], y[below], '-r', label='Tackles', markevery=10)
+    plt.plot(x[above], y[above], '--r',  markevery=10)
+    y = clean_sheets_ratio*x
+    plt.plot(x[below], y[below], '-g', label='Clean Sheets')
+    plt.plot(x[above], y[above], '--g')
+    y = successful_duels_ratio*x
+    plt.plot(x[below], y[below], '-b', label='Duels')
+    plt.plot(x[above], y[above], '--b')
+    y = fouls_committed_ratio*x
+    plt.plot(x[below], y[below], '-m', label='Fouls Committed')
+    plt.plot(x[above], y[above], '--m')
+    plt.xlabel('Appearances')
+    plt.ylabel('Statistics')
+    plt.legend(loc='upper left')
+    ax.set_facecolor('aliceblue')
+    plt.show()
+
+
+def keepersGraph(df):
+    appearances = df['appearances']
+    saves_ratio = df['saves'] / appearances
+    clean_sheets_ratio = df['clean_sheets'] / appearances
+    penalties_saved_ratio = df['penalties_saved'] / appearances
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    x = np.linspace(0, appearances + 200, 50)
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+    xthresh = appearances
+
+    below = x <= appearances
+    above = x > appearances - 40
+    y = saves_ratio*x
+    plt.plot(x[below], y[below], '-r', label='Saves', markevery=10)
+    plt.plot(x[above], y[above], '--r',  markevery=10)
+    y = clean_sheets_ratio*x
+    plt.plot(x[below], y[below], '-g', label='Clean Sheets')
+    plt.plot(x[above], y[above], '--g')
+    y = penalties_saved_ratio*x
+    plt.plot(x[below], y[below], '-b', label='Penalties Saved')
+    plt.plot(x[above], y[above], '--b')
+    plt.xlabel('Appearances')
+    plt.ylabel('Statistics')
+    plt.legend(loc='upper left')
+    ax.set_facecolor('aliceblue')
+    plt.show()
+
+
+def playerGraphs(player_id):
+
+    df = getPlayerInfo(player_id)
+
     # Goalkeepers Rating
     if (df['position_id'] == 1):
-        pass
+        keepersGraph(df)
 
     # Defenders Rating
     elif (df['position_id'] == 2):
-        pass
+        defendersGraph(df)
 
     elif (df['position_id'] == 3):
-        pass
+        attackersGraph(df)
 
     # Strikers/Wingers Rating
     # Goal ratio, assist ratio, successful dribbles ratio, successful crosses ratio, total passes ratio
     elif (df['position_id'] == 4):
-        pass        
+        attackersGraph(df)
 
 
-playerGraphs(580) # Cristiano Ronaldo
-# playerGraphs(184798) # Lionel Messi
-# playerGraphs(184941) # Sergio Ramos
-# playerGraphs(30594) # Alex Sandro
-# playerGraphs(186029) # Keylor Navas
+# playerGraphs(580)  # Cristiano Ronaldo
+# playerGraphs(184798)  # Lionel Messi
+""" playerGraphs(184941) """  # Sergio Ramos
+playerGraphs(30594)  # Alex Sandro
+# playerGraphs(186029)  # Keylor Navas
