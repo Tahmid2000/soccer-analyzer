2754d054cf1dd5c5c99732c2ca014f4f3cf1232e
Raiyan Mahbub <50161218+mahbu006@users.noreply.github.com>
2021-02-27 11:56:42 -0600

    added description

--- client/src/components/AboutPage/AboutPage.js
+++ client/src/components/AboutPage/AboutPage.js
@@ -10,23 +10,23 @@ class AboutPage extends React.Component {
             <div className="col m1"></div>
             <div className="col s12 m4">
               <AboutCard
-                name="Raiyan Mahbub"
-                description="asdfsdf"
-                link="raiyan.jpg"
-                portfolio="https://raiyanmahbub.com"
-                linkedin="https://www.linkedin.com/in/raiyan-mahbub/"
-                github="https://github.com/mahbu006"
-              />
-            </div>
-            <div className="col m2"></div>
-            <div className="col s12 m4">
-              <AboutCard
                 name="Tahmid Imran"
                 description="I'm Computer Science student at UT Dallas, graduating in May 2022. I love building full-stack applications like this!"
                 link="tahmid.jpg"
                 portfolio="https://tahmidimran.com"
                 linkedin="https://www.linkedin.com/in/tahmidimran/"
                 github="https://github.com/Tahmid2000"
+              />
+            </div>
+            <div className="col m2"></div>
+            <div className="col s12 m4">
+            <AboutCard
+                name="Raiyan Mahbub"
+                description="I am a Computer Engineering student at University of Minnesota, graduating in December 2021. "
+                link="raiyan.jpg"
+                portfolio="https://raiyanmahbub.com"
+                linkedin="https://www.linkedin.com/in/raiyan-mahbub/"
+                github="https://github.com/mahbu006"
               />
             </div>
             <div className="col m1"></div>
